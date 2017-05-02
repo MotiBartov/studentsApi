29fa9a0a7649554d3368aee940753a1cf9712837
motibartov <moti0375@gmail.com>
2017-05-02 21:54:11 +0300

    Add new images

--- Repository.php
+++ Repository.php
@@ -14,9 +14,10 @@
 
 
          $s1 = Array($this->firstNameKey => 'Moti', $this->lastNameKey => 'Bartov', $this->idKey => '031992654', $this->imageKey => 'moti.jpg');
-         $s2 = Array($this->firstNameKey => 'Donald', $this->lastNameKey => 'Trump', $this->idKey => '555', $this->imageKey => 'trump.jpg');
+         $s2 = Array($this->firstNameKey => 'Mickey', $this->lastNameKey => 'Mouse', $this->idKey => '555', $this->imageKey => 'mickey.jpg');
+         $s3 = Array($this->firstNameKey => 'Donald', $this->lastNameKey => 'Douck', $this->idKey => '555', $this->imageKey => 'donald.jpg');
          
-         $this->repo = Array($s1, $s2);
+         $this->repo = Array($s1, $s2, $s3);
      }
 
      function getStudents(){
